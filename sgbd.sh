86937d46ab58160ea5e713cce346f04d5ddb0597
Igor Nobre <ferreiranobre@outlook.com>
2022-05-15 01:58:15 -0300

    postgresql script install

--- sgbd.sh
+++ sgbd.sh
@@ -7,7 +7,8 @@ sudo gdebi libssl.deb
 
 
 sudo apt-get update && LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive
-sudo gdebi postgres-client.deb && sudo gdebi postgresql.deb
+sudo gdebi postgres-client.deb 
+sudo gdebi postgresql.deb
 
 
 # /etc/ssl/private can't be accessed from within container for some reason
