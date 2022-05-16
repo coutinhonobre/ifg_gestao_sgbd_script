d7e224d47e4cc1636095dda306e02acd0371d496
Igor Nobre <ferreiranobre@outlook.com>
2022-05-15 23:14:20 -0300

    install deb

--- sgbd.sh
+++ sgbd.sh
@@ -2,10 +2,13 @@
 
 sudo apt-get update && apt-get install -y sudo wget ca-certificates
 sudo apt-get install -y libpq5 libpq-dev
+chmod -R 0700 ./libssl.deb
 sudo dpkg -i libssl.deb
 
 
 sudo apt-get update && LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive
+chmod -R 0700 ./postgresql.deb
+chmod -R 0700 ./postgresql-client.deb
 sudo dpkg -i postgres-client.deb 
 sudo dpkg -i postgresql.deb
 
