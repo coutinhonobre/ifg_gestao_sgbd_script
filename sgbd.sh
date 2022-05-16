20e20f9786710dd43a2dcedb841d04b6afd8ec22
Igor Nobre <ferreiranobre@outlook.com>
2022-05-15 22:43:26 -0300

    script base postgres

--- sgbd.sh
+++ sgbd.sh
@@ -11,14 +11,4 @@ sudo gdebi postgres-client.deb
 sudo gdebi postgresql.deb
 
 
-# /etc/ssl/private can't be accessed from within container for some reason
-# (@andrewgodwin says it's something AUFS related)
-sudo  mkdir /etc/ssl/private-copy; mv /etc/ssl/private/* /etc/ssl/private-copy/; rm -r /etc/ssl/private; mv /etc/ssl/private-copy /etc/ssl/private; chmod -R 0700 /etc/ssl/private; chown -R postgres /etc/ssl/private
-
-## Add over config files
-cp postgresql.conf /etc/postgresql/8.3/main/postgresql.conf
-cp pg_hba.conf /etc/postgresql/8.3/main/pg_hba.conf
-sudo chown postgres:postgres /etc/postgresql/8.3/main/*.conf
-cp init-postgresql /usr/local/bin/init-postgresql
-sudo chmod +x /usr/local/bin/init-postgresql
 
