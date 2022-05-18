f99d0d78fbb0b019b3a32b617ae75dd03e9aaa64
Igor Nobre <ferreiranobre@outlook.com>
2022-05-18 00:19:52 -0300

    permission file

--- sgbd.sh
+++ sgbd.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+sudo chmod -R 777 ./
 sudo apt-get update && apt-get install -y sudo wget ca-certificates
 sudo apt-get install -y libpq5 libpq-dev
 sudo apt-get install ssl-cert
-sudo chmod -R 0700 ./libssl.deb
 sudo dpkg -i libssl.deb
 
 
 sudo apt-get update && LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive
-sudo chmod -R 0700 ./postgresql.deb
-sudi chmod -R 0700 ./postgresql-client.deb
+sudo chmod -R 0700 ./
 sudo dpkg -i ./postgresql-client.deb
 sudo dpkg -i ./postgresql.deb
 
