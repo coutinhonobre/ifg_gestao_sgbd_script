1aad8431ac6b3cae9d43fb1f8cb347b53ef00b1d
Igor Nobre <ferreiranobre@outlook.com>
2022-05-18 01:21:21 -0300

    script generate

--- sgbd.sh
+++ sgbd.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo chmod -R 777 ./ &&
-sudo apt-get update && apt-get install -y sudo wget ca-certificates &&
+sudo apt-get install -y sudo wget ca-certificates &&
 sudo dpkg -i libpq5.deb &&
 sudo dpkg -i libpq-dev.deb &&
 sudo apt-get install ssl-cert && sudo dpkg -i libssl.deb &&
 
 
-sudo apt-get update && LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive &&
+LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive &&
 sudo apt-get install postgresql-client-common &&
 sudo dpkg -i ./postgresql-client.deb && sudo dpkg -i ./postgresql.deb
 
