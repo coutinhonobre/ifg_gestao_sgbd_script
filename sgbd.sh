dd59aad84e55b7a9950cf730bd61354c2e364ee7
Igor Nobre <ferreiranobre@outlook.com>
2022-05-18 00:53:43 -0300

    install multiple lines

--- sgbd.sh
+++ sgbd.sh
@@ -4,9 +4,6 @@ sudo chmod -R 777 ./ &&
 sudo apt-get update && apt-get install -y sudo wget ca-certificates &&
 sudo apt-get install -y libpq5 libpq-dev &&
 sudo apt-get install ssl-cert && sudo dpkg -i libssl.deb &&
-
-
-sudo apt-get update && LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive &&
 sudo dpkg -i ./postgresql-client.deb && sudo dpkg -i ./postgresql.deb
 
 
